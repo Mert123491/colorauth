6b72076c71700c7b63c4145de268472063530a2d
blasdasd <67392350+Mert123491@users.noreply.github.com>
2025-03-23 17:04:26 +0300

    Update server.py

--- server.py
+++ server.py
@@ -1,5 +1,5 @@
-from flask import Flask, request
 import os
+from flask import Flask, request
 
 app = Flask(__name__)
 
@@ -8,12 +8,11 @@ def auth():
     key = request.args.get('key')
     machine_id = request.args.get('machine_id')
 
-    if key == "ABC123" and machine_id == "XYZ987":
-        return "[status] result=True"
+    if key == "ABC123" and machine_id == "MACHINE01":
+        return "result=True"
     else:
-        return "[status] result=True"
+        return "result=True"
 
-# 🔥 BURASI ÖNEMLİ!
 if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port)
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host='0.0.0.0', port=port)
